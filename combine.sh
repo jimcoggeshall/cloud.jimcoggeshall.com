48fd79b00a08b7a1b4fc4ab01df19cefb0f173d5
Jim Coggeshall <jim@jimcoggeshall.com>
2020-09-26 20:45:21 -0700

    upload others too

--- combine.sh
+++ combine.sh
@@ -3,5 +3,7 @@
 mkdir -p var/output && chmod -R 777 var/output
 docker build -t combine:latest -f Dockerfile.combine .
 docker run --shm-size='2gb' --network="host" -v $(pwd)/var/output:/output --rm combine:latest
-cp $(pwd)/var/output/TodaysPaper.pdf $(pwd)/var/www/cloud.jimcoggeshall.com/TodaysPaper.pdf
-
+find $(pwd)/var/output -name '*.pdf' |\
+while read f; do
+    cp "$f" "$(pwd)/var/www/cloud.jimcoggeshall.com/"
+done
